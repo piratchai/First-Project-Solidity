064c36828cb99847862298e10fc41e21d5e2ecad
bitbucketOwn <piratcha.age@gmail.com>
2021-06-03 21:45:56 +0700

    add code third file.

--- 3_MappingStructsExample.sol
+++ 3_MappingStructsExample.sol
@@ -2,5 +2,15 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract MappingStructsExample {
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
     
+    function sendMoney() public payable {
+        
+    }
+    
+    function withdrawAllMoney(address payable _to) public {
+        _to.transfer(address(this).balance);
+    }
 }
